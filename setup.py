cc5265fde85e38013e928ce26f17261dd93c3682
Tao Peng <ptpttt@gmail.com>
2013-03-17 18:04:03 +0100

    0.0.4 released

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
         return f.read()
 
 setup(name='ptwit',
-      version='0.0.3',
+      version='0.0.4',
       description='A simple twitter command line client',
       long_description=readme(),
       classifiers=[
